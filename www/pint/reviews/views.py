716323ba69664add63d0c244615a9ff8409408a9
fati <fati13592@users.noreply.github.com>
2015-05-08 09:34:23 +0200

    Añadido logout django

--- www/pint/reviews/views.py
+++ www/pint/reviews/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 from reviews.models import Product,Review
 from reviews.forms import UserForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -108,4 +109,17 @@ def user_login(request):
         # No context variables to pass to the template system, hence the
         # blank dictionary object...
         return render(request, 'reviews/login.html', {})
+
+
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+
+    # Take the user back to the homepage.
+    return HttpResponseRedirect('/reviews/')
+
+
+
+
 
