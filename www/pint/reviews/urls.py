4773d7e269ea68bd0a520d26180d20e7fe79c813
fati <fati13592@users.noreply.github.com>
2015-06-02 10:02:55 +0200

    añadido bootstrap

--- www/pint/reviews/urls.py
+++ www/pint/reviews/urls.py
@@ -4,8 +4,8 @@ from reviews import views
 urlpatterns=patterns('',
 	url(r'^$', views.index, name='index'),
 	url(r'^(?P<product_id>\d+)/$',views.detail,name='detail'),
-	url(r'^register/$', views.register, name='register'),
-	url(r'^login/$', views.user_login, name='login'),
+	url(r'^user/register/$', views.register, name='register'),
+	url(r'^user/login/$', views.user_login, name='login'),
 	url(r'^logout/$', views.user_logout, name='logout'),
 	url(r'^photos/$', views.find_photos, name='photos'),
 )
